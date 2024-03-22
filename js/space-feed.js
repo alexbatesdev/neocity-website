604078b3eb50724e23f88ef09e752503bce7e984
Alex Bates <alexbates.dev@gmail.com>
2024-03-21 23:34:21 -0600

    ISS update

--- js/space-feed.js
+++ js/space-feed.js
@@ -1,14 +1,11 @@
-const europaClick = () => {
-    let clipperFeed = document.getElementById('clipper-feed');
+const toggleMenu = () => {
     let infoFrame = document.getElementsByClassName('info-frame')[0];
     let miniMenu = document.getElementsByClassName('mini-menu')[0];
 
-    if (clipperFeed.classList.contains('hidden')) {
-        clipperFeed.classList.remove('hidden');
+    if (miniMenu.classList.contains('hidden')) {
         infoFrame.classList.add('hidden');
         miniMenu.classList.remove('hidden');
     } else {
-        clipperFeed.classList.add('hidden');
         infoFrame.classList.remove('hidden');
         miniMenu.classList.add('hidden');
     }
