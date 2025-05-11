c8d4f2f65edc9827b706c1ff260d5b80d1f9624e
Alex Bates <alexbates.dev@gmail.com>
2025-05-11 16:36:39 -0600

    Sun and Moon update

--- js/index.js
+++ js/index.js
@@ -41,8 +41,6 @@ const getTrack = async () => {
     }
 };
 
-
-
 setInterval(() => { getTrack(); }, 10000);
 getTrack();
 
@@ -51,11 +49,25 @@ const playSoundEffect = (filename) => {
     audio.play();
 }
 
+// This section here makes the globe in the background send its scroll to the main body
+// scrolling would cause the mouse to be positioned over the globe, which would cause the globe to stop the body from scrolling
+const el = document.querySelector('.globe');
+const target = document.querySelector('.body'); // the element that should actually scroll
+el.addEventListener('wheel', (e) => {
+    console.log('wheel event', e);
+    target.scrollTop += e.deltaY;   // vertical scroll
+    target.scrollLeft += e.deltaX;  // horizontal scroll
+  e.preventDefault(); // Prevent scrolling
+}, { passive: false }); // passive: false is required to allow preventDefault()
+// section end
+
 window.addEventListener('load', () => {
     const parallaxElements = document.querySelectorAll('.plax');
-    console.log(parallaxElements);
     const scrollContainer = document.querySelector('.retro-scrollbar');
-    console.log(scrollContainer);
+    parallaxElements.forEach(element => {
+        // console.log('parallax element', element);
+        calculateParallax(element);
+    });
     scrollContainer.addEventListener('scroll', () => {
         parallaxElements.forEach(element => {
             // console.log('parallax element', element);
@@ -65,38 +77,46 @@ window.addEventListener('load', () => {
 });
 
 function calculateParallax(element) {
+    console.log('-----------------');
     const scrollContainer = document.querySelector('.retro-scrollbar');
-    const PARALLAX_SENSITIVITY = 50;
+    const PARALLAX_SENSITIVITY = 25;
     
     // Get the z-index from the class (e.g., "z50")
     let zIndex = 0;
     element.classList.forEach(className => {
         if (className.startsWith('z')) {
             zIndex = parseInt(className.replace('z', ''), 10);
+            element.style.zIndex = zIndex; // Set the z-index of the element
         }
     });
-    const parallaxSpeed = (zIndex / 100) * PARALLAX_SENSITIVITY;
+    // Calculate the parallax speed based on the z-index
+    // more negative values will move slower and more positive values will move faster 
+    const depth = 100 + zIndex; // ensure no division by zero and avoid negatives
+    const parallaxSpeed = depth / PARALLAX_SENSITIVITY; // Adjust the sensitivity as needed
 
     console.log(`zIndex: ${zIndex}`);
-    // log scrollContainer scrollHeight
-    console.log(`scrollContainer scrollHeight: ${scrollContainer.scrollHeight}`);
-    // log scrollContainer scrollTop
-    console.log(`scrollContainer scrollTop: ${scrollContainer.scrollTop}`);
-    // log scrollContainer scrollBottom
-    console.log(`scrollContainer scrollHeight - scrollTop - clientHeight: ${scrollContainer.scrollHeight - scrollContainer.scrollTop - scrollContainer.clientHeight}`);
-    // log scrollContainer clientHeight
-    console.log(`scrollContainer clientHeight: ${scrollContainer.clientHeight}`);
+    console.log(`depth: ${depth}`);
+    console.log(`parallaxSpeed: ${parallaxSpeed}`);
+    // // log scrollContainer scrollHeight
+    // console.log(`scrollContainer scrollHeight: ${scrollContainer.scrollHeight}`);
+    // // log scrollContainer scrollTop
+    // console.log(`scrollContainer scrollTop: ${scrollContainer.scrollTop}`);
+    // // log scrollContainer scrollBottom
+    // console.log(`scrollContainer scrollHeight - scrollTop - clientHeight: ${scrollContainer.scrollHeight - scrollContainer.scrollTop - scrollContainer.clientHeight}`);
+    // // log scrollContainer clientHeight
+    // console.log(`scrollContainer clientHeight: ${scrollContainer.clientHeight}`);
     // log normalized scroll position
-    const normalizedScrollPosition = 1 - (scrollContainer.scrollHeight - scrollContainer.scrollTop - scrollContainer.clientHeight) / (scrollContainer.scrollHeight - scrollContainer.clientHeight);
+    const normalizedScrollPosition = 1 - (scrollContainer.scrollTop / (scrollContainer.scrollHeight - scrollContainer.clientHeight));
+
     console.log(`normalizedScrollPosition: ${normalizedScrollPosition}`);
 
     // Calculate the new position of the element based on the scroll position and parallax speed
     const newPosition = (normalizedScrollPosition - 0.5) * parallaxSpeed * 100;
-    console.log(`newPosition: ${newPosition}`);
+    // console.log(`newPosition: ${newPosition}`);
     // Apply the new position to the element
     const computedStyle = window.getComputedStyle(element);
-    element.style.transform = `translateY(${newPosition}%)`;
-    console.log(computedStyle.transform);
+    element.style.transform = `translateY(${newPosition}px)`;
+    // console.log(computedStyle.transform);
 // matrix(1, 0, 0, 1, 0, 42.6357)
 // matrix(0.866025, 0.5, -0.5, 0.866025, 0, 0)
 
