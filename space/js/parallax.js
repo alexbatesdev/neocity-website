42697591024d9fef126460425033cea5624d193c
Alex Bates <alexbates.dev@gmail.com>
2026-02-18 21:15:29 -0700

    Solidified parallax I think

--- space/js/parallax.js
+++ space/js/parallax.js
@@ -8,20 +8,24 @@ export class ParallaxScene {
 
   update() {
     const scrollY = window.scrollY;
-    this.elements.forEach(el => this.applyParallax(el, scrollY));
+    const scrollX = window.scrollX - (document.documentElement.scrollWidth - window.innerWidth) / 2;
+    this.elements.forEach(el => this.applyParallax(el, scrollY, scrollX));
   }
 
-  applyParallax(element, scrollY) {
+  applyParallax(element, scrollY, scrollX) {
     let zIndex = element.style.zIndex
       || element.computedStyleMap().get('z-index').value;
     const speed = (parseInt(zIndex) / 100) * this.sensitivity;
 
-    let xOffset = 0;
-    let yOffset = scrollY * speed;
+    let xOffset = -(scrollX * speed);
+    let yOffset = -(scrollY * speed);
 
     if (element.classList.contains('noplax-y')) yOffset = 0;
     if (element.classList.contains('noplax-x')) xOffset = 0;
 
-    element.style.transform = `translate(${xOffset}px, ${yOffset}px) scale(${1 + speed})`;
+    const scale = element.classList.contains('noplax-scale')
+      ? 1
+      : (1 + speed);
+    element.style.transform = `translate(${xOffset}px, ${yOffset}px) scale(${scale})`;
   }
 }
